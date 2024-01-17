6083d7b1e34d2e46002672ea99036daa5cc9184b
Bhuvan Soni <soni.bhuvan@satsure.co>
2024-01-17 14:09:03 +0530

    added additional param in user_routes

--- api/user_routes.py
+++ api/user_routes.py
@@ -3,35 +3,42 @@ from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identi
 from api.models import User
 from api.main import db
 
-auth_bp = Blueprint('auth', __name__)
+auth_bp = Blueprint("auth", __name__)
 
-@auth_bp.route('/login', methods=['POST'])
+
+@auth_bp.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
-    email = data.get('email')
-    password = data.get('password')
+    email = data.get("email")
+    password = data.get("password")
 
     user = User.query.filter_by(email=email, is_admin=True).first()
 
     if user and user.password == password:
         access_token = create_access_token(identity=user.user_id)
-        return jsonify(access_token=access_token), 200
+        return (
+            jsonify(
+                {"access_token": access_token, "email": user.email, "name": user.name}
+            ),
+            200,
+        )
     else:
-        return jsonify({'message': 'Invalid credentials'}), 401
+        return jsonify({"message": "Invalid credentials"}), 401
 
-@auth_bp.route('/create_user', methods=['POST'])
+
+@auth_bp.route("/create_user", methods=["POST"])
 def create_user():
     data = request.get_json()
-    name = data.get('name')
-    email = data.get('email')
+    name = data.get("name")
+    email = data.get("email")
 
     existing_user = User.query.filter_by(email=email).first()
 
     if existing_user:
-        return jsonify({'message': 'User already exists'}), 200
+        return jsonify({"message": "User already exists"}), 200
     else:
         new_user = User(name=name, email=email)
 
         db.session.add(new_user)
         db.session.commit()
-        return jsonify({'message': 'User created successfully'}), 201
+        return jsonify({"message": "User created successfully"}), 201
